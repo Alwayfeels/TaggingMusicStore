636d0290d686f88160d9a2192060b36475440f28
Alwayfeels <547542652@qq.com>
2022-09-26 20:19:01 +0800

    add CORS config

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ app.use(Router)
 
 //设置跨域访问
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true")
     res.header("X-Powered-By",' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
